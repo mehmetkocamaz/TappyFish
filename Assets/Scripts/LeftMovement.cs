4f73fb31ebc6aa09aa2bf90601353b2bc28e5cf7
Mehmet Kocamaz <kocamazmehmet@hotmail.com>
2023-05-29 20:16:02 +0300

    Added Obstacle Scripts

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -9,20 +9,31 @@ public class LeftMovement : MonoBehaviour
     float speed;
     BoxCollider2D box;
     float groundWidth;
+    float obstacleWidth;
     // Start is called before the first frame update
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWidth = box.size.x;
+        if (gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidth = box.size.x;
+        }
+        else if (gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
-        if (transform.position.x <= -groundWidth)
+        if (gameObject.CompareTag("Ground"))
         {
+            if (transform.position.x <= -groundWidth)
+            {
             transform.position = new Vector2(transform.position.x + 2 * groundWidth, transform.position.y);
+            }
         }
     }
 }
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    public void InstantiateObstacle()
+    {
+        GameObject newObstacle = Instantiate(obstacle);
+        newObstacle.transform.position = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateObstacle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
